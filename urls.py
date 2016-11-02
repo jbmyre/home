f6b12708a2de9f7e4d87860e78796ad34803041f
Joel Buikema <joel.buikema@cbs.com>
2016-11-02 18:56:48 -0400

    added template

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+app_name = 'home'
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    #url(r'^send_command/(?P<box>[\w\-]+)/(?P<code>[\w\-]+)/$', views.send_command, name='send_commmand'),
+]
--- views.py
+++ views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import json
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home/home.html')
