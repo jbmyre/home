d178efa74e5bf5afe3d54578820799708be476f2
Joel <jbmyre@gmail.com>
2016-11-08 22:21:07 -0500

    added basic models

--- models.py
+++ models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Page(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    page = models.ForeignKey(
+        Page,
+        on_delete=models.CASCADE,
+        related_name="post"
+    )
+    text = models.TextField()
