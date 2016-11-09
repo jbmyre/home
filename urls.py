555740258bec2dc344218b06289dffb7b2e7c325
Joel <jbmyre@gmail.com>
2016-11-08 22:23:43 -0500

    removed $ from url regex

--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^', views.home, name='home'),
     #url(r'^send_command/(?P<box>[\w\-]+)/(?P<code>[\w\-]+)/$', views.send_command, name='send_commmand'),
 ]
